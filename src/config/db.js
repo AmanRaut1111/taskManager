8125e4a08b15b90ac9fadeaab1a5b52b9e03f9a5
Aman Raut <amanraut1111@gmail.com>
2025-12-28 21:34:52 +0530

    change in config

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connectDb = await mongoose.connect(
-      "mongodb+srv://amanraut7576_db_user:dZJ7fogIU0wi9xkM@cluster0.zjbnmxg.mongodb.net/ taskManger?retryWrites=true&w=majority"
+      "mongodb+srv://amanraut7576_db_user:dZJ7fogIU0wi9xkM@cluster0.zjbnmxg.mongodb.net/taskManger?retryWrites=true&w=majority"
     );
     console.log("Database Connected");
   } catch (error) {
